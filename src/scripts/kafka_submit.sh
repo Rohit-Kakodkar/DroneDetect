c1b13c8c29905724c6a1c77d402f370276c51ffd
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2019-06-19 15:15:46 -0400

    Added submit scripts

--- src/scripts/kafka_submit.sh
+++ src/scripts/kafka_submit.sh
@@ -1,4 +1,5 @@
-${HOME}/DroneDetect/src/kafka/kafka_producer.py /
---number_of_devices 300 /
---broker $KAFKA_CLUSTER_1:9092 $KAFKA_CLUSTER_2:9092 $KAFKA_CLUSTER_2:9092 /
---topic sensor-data-1
+${HOME}/DroneDetect/src/kafka/kafka_producer.py \
+--number_of_devices 300 \
+--broker $KAFKA_CLUSTER_1:9092 $KAFKA_CLUSTER_2:9092 $KAFKA_CLUSTER_2:9092 \
+--topic sensor-data-1 \
+/
