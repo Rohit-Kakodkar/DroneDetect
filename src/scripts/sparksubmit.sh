742697f8fd9ef3819d1810d75bf0586413eb5fd2
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2019-06-25 10:57:05 -0400

    Fixed submit script

--- src/scripts/sparksubmit.sh
+++ src/scripts/sparksubmit.sh
@@ -1,7 +1,7 @@
 spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.0\
  --master spark://${SPARK_MASTER}:7077\
  ${HOME}/DroneDetect/src/spark/sparkDirectStream.py\
- --broker '${BROKER}'\
+ --broker ${BROKER}\
  --topic sensor-data-1\
  --dbname ${DB_NAME}\
  --pusername ${PS_USER}\
