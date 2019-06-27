6161f3e1f47f5dc93885b8275f085dda44a67ca4
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2019-06-26 21:32:16 -0400

    Fixed flask

--- src/flask/app/views.py
+++ src/flask/app/views.py
@@ -56,6 +56,7 @@ def home():
 @app.route('/employeelogin')
 def employeelogin():
     tp = TopicPartition('crashed-devices',0)
+    print('GOT HERE')
     consumer = KafkaConsumer('crashed-devices', bootstrap_servers=['ec2-52-203-135-135.compute-1.amazonaws.com:9092',
                             'ec2-52-70-111-222.compute-1.amazonaws.com:9092', 'ec2-34-193-78-218.compute-1.amazonaws.com:9092'],
    						    enable_auto_commit=True, group_id='my-group',
