0f399e94ccb1662a784a2d4f269a38129a310527
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2019-06-26 22:20:48 -0400

    Fixed flask

--- src/spark/sparkDirectStream.py
+++ src/spark/sparkDirectStream.py
@@ -208,7 +208,7 @@ def process_drones(rdd):
         # latitudes = crashed_DF.select('latitude').collect()
         print(latitudes)
         for latitude in crashed_DF.rdd.collect():
-            print(row.device_id, row.latitude, row.longitude)
+            print(latitude)
         #     # data = dumps({  "device_id" : row.device_id,
         #     #                 "latitude" : row.latitude,
         #     #                 "longitude" : row.longitude}).encode('utf-8')
