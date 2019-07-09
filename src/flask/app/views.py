fc57b26c9591772230bb0e2f1dfe8e72bffcfea5
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2019-07-09 07:44:20 -0400

    fixed spark

--- src/flask/app/views.py
+++ src/flask/app/views.py
@@ -6,9 +6,6 @@ from kafka import KafkaConsumer, TopicPartition
 import argparse
 from json import loads
 
-latitudes = [40.6349833109, 40.6268903367, 40.5383258365, 40.6224222456, 40.8552825266, 40.739323065, 40.7340164399]
-longitudes = [-73.9674308662, -73.9463895297, -74.1492046912, -74.004704123, -73.8877158182, -73.7005708962, -73.9928895187]
-
 @app.route('/')
 @app.route('/home')
 def home():
@@ -66,31 +63,31 @@ def home():
                            )
 
 @app.route('/employeeportal')
-def employeeportal(latitudes, longitudes):
-    # tp = TopicPartition('crashed-devices',0)
-    # consumer = KafkaConsumer('crashed-devices', bootstrap_servers=['ec2-52-203-135-135.compute-1.amazonaws.com:9092',
-    #                         'ec2-52-70-111-222.compute-1.amazonaws.com:9092', 'ec2-34-193-78-218.compute-1.amazonaws.com:9092'],
-   	# 					    enable_auto_commit=True, group_id='my-group',
-   	# 					    auto_offset_reset = 'earliest',
-    #                         value_deserializer=lambda x: loads(x.decode('utf-8')))
-    # lastOffset = consumer.beginning_offsets([tp])[tp]
-    # latitudes = []
-    # longitudes = []
-    # i = 0
-    # for message in consumer:
-    #     i += 1
-    #     msg = message.value
-    #     latitudes.append(msg['latitude'])
-    #     longitudes.append(msg['longitude'])
-    #     print(latitudes, longitudes)
-    #     if i==1:
-    #         print("GOT HERE")
-    #         consumer.commit()
-    #         break
-    #
-    # consumer.close()
+def employeeportal():
+    tp = TopicPartition('crashed-devices',0)
+    consumer = KafkaConsumer('crashed-devices', bootstrap_servers=['ec2-52-203-135-135.compute-1.amazonaws.com:9092',
+                            'ec2-52-70-111-222.compute-1.amazonaws.com:9092', 'ec2-34-193-78-218.compute-1.amazonaws.com:9092'],
+   						    enable_auto_commit=True, group_id='my-group',
+   						    auto_offset_reset = 'earliest',
+                            value_deserializer=lambda x: loads(x.decode('utf-8')))
+    lastOffset = consumer.beginning_offsets([tp])[tp]
+    latitudes = []
+    longitudes = []
+    i = 0
+    for message in consumer:
+        i += 1
+        msg = message.value
+        latitudes.append(msg['latitude'])
+        longitudes.append(msg['longitude'])
+        print(latitudes, longitudes)
+        if i==1:
+            print("GOT HERE")
+            consumer.commit()
+            break
+
+    consumer.close()
 
     return render_template("employeeportal.html",
                            APIkey = 'AIzaSyD9e3Rdo8fGQq6hzaXkdsdQzv9Hy0rTolE',
-                           latitudes= latitudes.pop(),
-                           longitudes = longitudes.pop())
+                           latitudes= latitudes,
+                           longitudes = longitudes)
