cfa5c90611e2059b5729d0f5bf38e192bf5e9695
Rohit Kakodkar <kakodkar.rohit@gmail.com>
2019-07-10 10:47:19 -0400

    Fixed script to setup spark cluster

--- setup/spark_environment.sh
+++ setup/spark_environment.sh
@@ -14,6 +14,6 @@ peg sshcmd-cluster ${SPARK_CLUSTER} "sudo apt-get install bc"
 peg install ${SPARK_CLUSTER} hadoop
 peg service ${SPARK_CLUSTER} hadoop start
 peg install ${SPARK_CLUSTER} spark
-peg service ${SPARK_CLUSTER} spark start
+peg service ${SPARK_CLUSTER} spark star
 
 # your spark cluster should now be up and running
